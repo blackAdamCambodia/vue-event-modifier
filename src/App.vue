e49652f19aa262d57282e52f3c9848c931295b45
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-07-22 20:03:29 +0700

    setup markup

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,16 @@
 <script setup>
-import {ref} from "vue"
-const about = ref('Hello world')
-const links=[
-  {
-    name: 'Google',
-    url: "https://www.google.com",
-    about:"Google is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware."
-  },
-  {
-    name: 'Microsoft',
-    url: "https://www.microsoft.com",
-    about:"Microsoft Corporation is an American multinational technology company with headquarters in Redmond, Washington. It develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services."
-  },
-  {
-    name: 'Facebook',
-    url: "https://www.facebook.com",
-    about:"Facebook is an American online social media and social networking service based in Menlo Park, California, and a flagship service of the namesake company Facebook, Inc."
-  },
-  {
-    name:"Twitter",
-    url:"https://www.twitter.com",
-    about:"Twitter is an American microblogging and social networking service on which users post and interact with messages known as tweets. Registered users can post, like and retweet tweets, but unregistered users can only read them."
-  }
-]
 
-function setText(text){
-  about.value = text
-}
 </script>
 
 <template>
-<section class="container mx-auto flex items-center flex-col">
+ <section class="container mx-auto flex items-center flex-col">
         <h1 class="text-center text-2xl py-10">Events &amp; Reactivity in Vue.js</h1>
-        <!-- <h1 class="text-center text-2xl py-10">{{ about }}</h1> -->
-        <h2 class="mt-10">{{ about }}</h2>
-        <div class="container mx-auto flex space-x-5 justify-center m-[1000px]">
-          <a target="_blank"  
-          v-for=" link in links" 
-          :key="link.name" 
-          href="#"
-          @click.prevent="setText(link.about)"
-          >
-          {{ link.name }}</a>
-          
-        </div>
+        <h2 class="mt-10">Hello Vue 3</h2>
+        <form action="https://google.com/search">
+            <div class="container mx-auto flex space-x-5 justify-center m-5"><input name="q" type="text" class="border border-gray-500 bg-white p-5" placeholder="Press Space"></div>
+            <div class="container mx-auto flex space-x-5 justify-center m-5"><input name="text" type="text" class="border border-gray-500 bg-white p-5" placeholder="Type Something"></div><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Button </button><button class="bg-blue-500 ml-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Ctrol + Right Click </button>
+        </form>
+        <div class="bg-gray-200 w-[500px] h-80 mt-5 flex items-center justify-center"><button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"> Button </button></div>
     </section>
 </template>
 
